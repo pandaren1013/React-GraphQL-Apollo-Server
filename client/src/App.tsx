85c50b222d1a531046851c92a5887768cda43cb8
pandaren1013 <pandaren1013@outlook.com>
2023-11-07 17:25:40 +0900

    updated code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+import AuthProvider, { RequireAuth } from "./contexts/AuthProvider";
+
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
@@ -10,21 +11,56 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Create from "./components/create";
 import Chart from "./pages/chart";
- 
+
 const App = () => {
- return (
-   <div>
+  return (
+    <AuthProvider>
       <Navbar />
-     <Routes>
-       <Route path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/login" element={<Login />} />
-       <Route path="/register" element={<Register />} />
-       <Route path="/create" element={<Create />} />
-       <Route path="/chart" element={<Chart />} />
-     </Routes>
-   </div>
- );
+      <Routes>
+
+      <Route
+            path='/'
+            element={
+              <RequireAuth>
+                <RecordList />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/edit/:id"
+            element={
+              <RequireAuth>
+                <Edit />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/chart"
+            element={
+              <RequireAuth>
+                <Chart />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <RequireAuth>
+                <Create />
+              </RequireAuth>
+            }
+          />
+
+          {/* <Route path="auth/*" element={<Login />} /> */}
+        {/* <Route path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/create" element={<Create />} />
+        <Route path="/chart" element={<Chart />} /> */}
+      </Routes>
+    </AuthProvider>
+  );
 };
- 
+
 export default App;
